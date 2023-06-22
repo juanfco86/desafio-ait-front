23bd0467927db551d87ae6262665fbd75cfa8de3
juanfco86 <juanfco86@gmail.com>
2023-06-22 17:05:27 +0200

    fix: dropdownMenu

--- src/Components/Dropdown/Dropdown.jsx
+++ src/Components/Dropdown/Dropdown.jsx
@@ -1,6 +1,15 @@
 import Dropdown from 'react-bootstrap/Dropdown';
+import { useNavigate } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom'
 
-function dropdown() {
+function DropdownMenu() {
+    // const history = useHistory()
+    const navigate = useNavigate()
+    const section = (event, path) => {
+        event.preventDefault()
+        navigate(path)
+    }
+
     return (
         <Dropdown>
         <Dropdown.Toggle variant="primary" id="dropdown-basic">
@@ -8,17 +17,17 @@ function dropdown() {
         </Dropdown.Toggle>
 
         <Dropdown.Menu>
-            <Dropdown.Item href="/search/dogs">Dogs</Dropdown.Item>
-            <Dropdown.Item href="/search/cats">Cats</Dropdown.Item>
-            <Dropdown.Item href="/search/computers">Computers</Dropdown.Item>
-            <Dropdown.Item href="/search/starwars">Star Wars</Dropdown.Item>
-            <Dropdown.Item href="/search/sports">Sports</Dropdown.Item>
-            <Dropdown.Item href="/search/smoke">Smoke</Dropdown.Item>
-            <Dropdown.Item href="/search/run">Carrera</Dropdown.Item>
+            <Dropdown.Item onClick={(e) => section(e, "/search/dogs")}>Dogs</Dropdown.Item>
+            <Dropdown.Item onClick={(e) => section(e, "/search/cats")}>Cats</Dropdown.Item>
+            <Dropdown.Item onClick={(e) => section(e, "/search/computers")}>Computers</Dropdown.Item>
+            <Dropdown.Item onClick={(e) => section(e, "/search/starwars")}>Star Wars</Dropdown.Item>
+            <Dropdown.Item onClick={(e) => section(e, "/search/sports")}>Sports</Dropdown.Item>
+            <Dropdown.Item onClick={(e) => section(e, "/search/smoke")}>Smoke</Dropdown.Item>
+            <Dropdown.Item onClick={(e) => section(e, "/search/run")}>Carrera</Dropdown.Item>
         </Dropdown.Menu>
         </Dropdown>
     );
 }
 
-export default dropdown;
+export default DropdownMenu;
     
